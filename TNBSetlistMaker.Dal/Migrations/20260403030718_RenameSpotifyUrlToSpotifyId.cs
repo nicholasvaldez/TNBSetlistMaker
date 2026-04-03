705960095a35fbc30d23ed6f11ee7820e8689517
Nicholas Valdez <nicholastvaldez@gmail.com>
2026-04-02 22:12:01 -0500

    feat: Spotify OAuth and TrackedPlaylist schema update

---
+++ TNBSetlistMaker.Dal/Migrations/20260403030718_RenameSpotifyUrlToSpotifyId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TNBSetlistMaker.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameSpotifyUrlToSpotifyId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SpotifyUrl",
+                table: "TrackedPlaylists",
+                newName: "SpotifyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SpotifyId",
+                table: "TrackedPlaylists",
+                newName: "SpotifyUrl");
+        }
+    }
+}
--- TNBSetlistMaker.Domain/Entities/TrackedPlaylist.cs
+++ TNBSetlistMaker.Domain/Entities/TrackedPlaylist.cs
@@ -2,6 +2,6 @@ public class TrackedPlaylist
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
-    public string SpotifyUrl { get; set; } = string.Empty;
+    public string SpotifyId { get; set; } = string.Empty;
     public DateTime? LastSynced { get; set; }
 }
